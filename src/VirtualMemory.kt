f70630f88f5e07a588d5d3ebe87f1fd362ad0938
receed <efremov-343@yandex.ru>
2020-09-18 12:52:11 +0300

    Add LRU implementation

--- src/VirtualMemory.kt
+++ src/VirtualMemory.kt
@@ -1,3 +1,6 @@
+import java.util.*
+import kotlin.test.currentStackTrace
+
 fun FIFO(numPages: Int, numFrames: Int, accessedPages: List<Int>): Array<Int?> {
     val pageInMemory = (0..numPages).map {false}.toMutableList()
     val pageInFrame = arrayOfNulls<Int>(numFrames + 1)
@@ -20,8 +23,53 @@ fun FIFO(numPages: Int, numFrames: Int, accessedPages: List<Int>): Array<Int?> {
     return frameToSubstitute
 }
 
+class C: Comparator<Pair<Int, Int>> {
+    override fun compare(p0: Pair<Int, Int>?, p1: Pair<Int, Int>?): Int {
+        if (p0 == null || p1 == null)
+            return 0
+        if (p0.first != p1.first)
+            return p0.first - p1.first
+        return p0.second - p1.second
+    }
+}
+
+fun LRU(numPages: Int, numFrames: Int, accessedPages: List<Int>): Array<Int?> {
+    val frameOfPage = arrayOfNulls<Int>(numPages + 1)
+    val lastUsed = (0..numPages).map {-1}.toMutableList()
+    val pageInFrame = arrayOfNulls<Int>(numFrames + 1)
+//    val substituteCandidates = TreeSet<Pair<Int, Int>>((1..numFrames).map {-1 to it})
+    val substituteCandidates = (1..numFrames).map {-1 to it}.toSortedSet(C())
+    val frameToSubstitute = arrayOfNulls<Int>(accessedPages.size)
+    for ((accessedIndex, page) in accessedPages.withIndex()) {
+        val oldFrame = frameOfPage[page]
+        if (oldFrame != null) {
+            substituteCandidates.remove(lastUsed[page] to page)
+            lastUsed[page] = accessedIndex
+            substituteCandidates.add(accessedIndex to oldFrame)
+        }
+        else {
+            val currentFrame = substituteCandidates.first().second
+            substituteCandidates.remove(substituteCandidates.first())
+            val oldPage = pageInFrame[currentFrame]
+            if (oldPage != null)
+                frameOfPage[oldPage] = null
+            frameToSubstitute[accessedIndex] = currentFrame
+            pageInFrame[currentFrame] = page
+            frameOfPage[page] = currentFrame
+            lastUsed[page] = accessedIndex
+            substituteCandidates.add(accessedIndex to currentFrame)
+        }
+    }
+    return frameToSubstitute
+}
+
 fun main() {
-    val res = FIFO(5, 3, listOf(1, 2, 5, 3, 2, 1, 4, 2, 5))
-    for (i in res)
+    val accessed = listOf(1, 2, 5, 3, 2, 1, 4, 2, 5)
+    val resFIFO = FIFO(5, 3, accessed)
+    for (i in resFIFO)
+        println(i)
+    println()
+    val resLRU = LRU(5, 3, accessed)
+    for (i in resLRU)
         println(i)
 }
