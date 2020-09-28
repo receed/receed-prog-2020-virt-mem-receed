9cf5c82c8cf4521abd9e7a64b7742c3abd5e65a1
receed <efremov-343@yandex.ru>
2020-09-28 22:40:46 +0300

    Add test for countScore

--- src/VirtualMemoryKtTest.kt
+++ src/VirtualMemoryKtTest.kt
@@ -4,18 +4,27 @@ import java.io.File
 
 internal class VirtualMemoryKtTest {
     private val task1 = Task(5, 3, listOf(1, 3, 2, 4, 1, 4, 5, 3, 2))
+
     @Test
     fun testFIFO() {
         assertArrayEquals(arrayOf(1, 2, 3, 1, 2, null, 3, 1, 2), FIFO.apply(task1))
     }
+
     @Test
     fun testLRU() {
         assertArrayEquals(arrayOf(1, 2, 3, 1, 2, null, 3, 2, 1), LRU.apply(task1))
     }
+
     @Test
     fun testOPT() {
         assertArrayEquals(arrayOf(1, 2, 3, 3, null, null, 1, null, 1), OPT.apply(task1))
     }
+
+    @Test
+    fun countScore() {
+        assertEquals(3, countScore(arrayOf(3, 6, null, 1, null)))
+    }
+
     @Test
     fun runFromFiles() {
         val files = arrayOf("data/test1", "data/test2")
