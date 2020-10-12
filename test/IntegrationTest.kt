94b551ff758abdb5e4588126a7a789b11455dc8f
receed <efremov-343@yandex.ru>
2020-10-12 23:01:11 +0300

    Separate integration and unit tests

---
+++ test/IntegrationTest.kt
@@ -0,0 +1,25 @@
+import FIFOStrategy
+import LRUStrategy
+import OPTStrategy
+import Task
+import calculateSubstitution
+import countScore
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import runFromFiles
+import java.io.File
+import org.junit.jupiter.api.Tag
+
+@Tag("integrationTest")
+internal class IntegrationTest {
+    @Test
+    fun runFromFiles() {
+        val files = arrayOf("data/test1", "data/test2")
+        runFromFiles(files)
+        for (file in files) {
+            val expected = File("$file.a").readLines()
+            val result = File("$file.out").readLines()
+            assertEquals(expected, result)
+        }
+    }
+}
--- test/VirtualMemoryKtTest.kt
+++ test/VirtualMemoryKtTest.kt
@@ -1,5 +1,3 @@
-package test.kotlin
-
 import FIFOStrategy
 import LRUStrategy
 import OPTStrategy
@@ -8,9 +6,11 @@ import calculateSubstitution
 import countScore
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Tag
 import runFromFiles
 import java.io.File
 
+@Tag("unitTest")
 internal class VirtualMemoryKtTest {
     private val task1 = Task(5, 3, listOf(1, 3, 2, 4, 1, 4, 5, 3, 2))
 
@@ -32,16 +32,5 @@ internal class VirtualMemoryKtTest {
     @Test
     fun countScore() {
         assertEquals(3, countScore(arrayOf(3, 6, null, 1, null)))
-    }
-
-    @Test
-    fun runFromFiles() {
-        val files = arrayOf("data/test1", "data/test2")
-        runFromFiles(files)
-        for (file in files) {
-            val expected = File("$file.a").readLines()
-            val result = File("$file.out").readLines()
-            assertEquals(expected, result)
-        }
     }
 }
