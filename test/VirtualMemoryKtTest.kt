43b93e06ee93e4776d72bd9ae66b9dde07d7fea7
receed <efremov-343@yandex.ru>
2020-10-15 19:41:07 +0300

    Add big tests for algorithms

--- test/VirtualMemoryKtTest.kt
+++ test/VirtualMemoryKtTest.kt
@@ -11,10 +11,25 @@ import java.io.File
 @Tag("unitTest")
 internal class VirtualMemoryKtTest {
     private val task1 = Task(5, 3, listOf(1, 3, 2, 4, 1, 4, 5, 3, 2))
+    private val largePages = 40
+    private val largeFrames = 32
+    private val largeAccesses = 1000000
+    private val largeTask = Task(largePages, largeFrames, generateAccessSequence(largePages, largeAccesses))
+
+    @Test
+    fun generateAccessSequence() {
+        assertEquals(largeAccesses, largeTask.accessedPages.size)
+        largeTask.accessedPages.forEach { assert(it in 1..largePages) }
+    }
 
     @Test
     fun testFIFO() {
         assertArrayEquals(arrayOf(1, 2, 3, 1, 2, null, 3, 1, 2), FIFOStrategy(task1).apply())
+    }
+
+    @Test
+    fun largeTestFIFO() {
+        assert(isValidSubstitution(largePages, largeFrames, largeTask.accessedPages, FIFOStrategy(largeTask).apply()))
     }
 
     @Test
@@ -23,8 +38,18 @@ internal class VirtualMemoryKtTest {
     }
 
     @Test
+    fun largeTestLRU() {
+        assert(isValidSubstitution(largePages, largeFrames, largeTask.accessedPages, LRUStrategy(largeTask).apply()))
+    }
+
+    @Test
     fun testOPT() {
         assertArrayEquals(arrayOf(1, 2, 3, 3, null, null, 1, null, 1), OPTStrategy(task1).apply())
+    }
+
+    @Test
+    fun largeTestOPT() {
+        assert(isValidSubstitution(largePages, largeFrames, largeTask.accessedPages, OPTStrategy(largeTask).apply()))
     }
 
     @Test
